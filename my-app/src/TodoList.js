d959bf8e1257f89a0ebe1da12840cd23cd380680
wangsong <wshappyday@sina.com>
2018-10-29 16:57:00 +0800

    2018.10.29

--- my-app/src/TodoList.js
+++ my-app/src/TodoList.js
@@ -6,29 +6,40 @@ class TodoList extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      list: [
-        'learn react',
-        'learn english',
-        'sdfsdf'
-      ]
+      list: [],
+      inputValue: ''
     }
   }
   handleBtnClick () {
-    console.log(this)
     this.setState({
-      list: [...this.state.list, 'hello world']
+      list: [...this.state.list, this.state.inputValue],
+      inputValue: ''
+    })
+  }
+
+  handleInputChange (e) {
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+
+  handleItemClick (index) {
+    const list = [...this.state.list];
+    list.splice(index, 1);
+    this.setState({
+      list: list
     })
   }
 
   render () {
     return (
       <div>
-        <input />
+        <input value={this.state.inputValue} onChange={this.handleInputChange.bind(this)} />
         <button onClick={this.handleBtnClick.bind(this)}>add</button>
         <ul>
           {
-            this.state.list.map((item, key) => {
-              return <li key={key}>{item}</li>
+            this.state.list.map((item, index) => {
+              return <li onClick={this.handleItemClick.bind(this, index)} key={index}>{item}</li>
             })
           }
         </ul>
